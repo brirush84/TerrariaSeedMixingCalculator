9bfdda7b5bdc04a8a21acf7cb249d8a57c6aa592
brirush84 <brushton84@gmail.com>
2026-02-27 22:40:07 -0600

    Update combos.js

--- src/data/combos.js
+++ src/data/combos.js
@@ -2,17 +2,28 @@ addCombo([seedOr([paintEverythingGray, paintEverythingNegative]), secretSeedsCou
     ['All blocks are painted with Echo Coating except for the blocks that outline chunks of land.']
 );
 
+
+addCombo([notTheBees, seedNot(theConstant), seedNot(skyblock), seedNot(drunk), seedNot(celebrationMk10), seedNot(remix)],
+    ['Most of the world is converted to Jungle.']
+);
+
+addCombo([notTheBees, seedOr([theConstant, drunk, celebrationMk10]), seedNot(remix), seedNot(skyblock)],
+    ['Most of the world is converted to Jungle, except for the middle portion of the world.']
+);
+
+addCombo([notTheBees, remix],
+    ['A rectangular strip of hive spawns near the top of the world, stretching across almost the entire world (except the oceans). A strip of Crispy Honey Block spawns underneath that.']
+);
+
+
 addCombo([notTheBees, seedNot(theConstant), seedNot(remix)],
-    ['One ocean is honey.']
+    ['One ocean is Honey, with its sand converted to Honey Blocks.']
 );
 
 addCombo([notTheBees, noTraps, seedNot(remix)],
-    ['Dart traps are replaced with Jungle Temple traps and Spikes are replaced by Wooden Spikes.']
+    ['Dart traps are replaced with Jungle Temple traps and Spikes are replaced by Wooden Spikes. Jungle Temple traps can be mined without a picksaw.']
 );
 
-addCombo([seedOr([notTheBees, noTraps, pooEverywhere]), seedNot(actuallyNoTraps), secretSeedsCount(0,3, actuallyNoTraps)],
-    ['Poo boulders may spawn.']
-);
 
 addCombo([celebrationMk10, noTraps],
     ['Extra rainbow boulders spawn.']
@@ -44,22 +55,6 @@ addCombo([drunk, seedNot(extraLivingTrees)],
 
 addCombo([seedNot(drunk), celebrationMk10, seedNot(extraLivingTrees)],
     ['A larger number of living trees than usual are added.']
-);
-
-addCombo([seedOr([skyblock, roundLandmasses, noSurface, notTheBees]), seedNot(theConstant)],
-    ['The Oasis mini biome cannot spawn.']
-);
-
-addCombo([seedOr([skyblock, roundLandmasses, noSurface]), theConstant],
-    ['The Oasis mini biome cannot spawn.']
-);
-
-addCombo([noTraps, forTheWorthy, seedNot(celebrationMk10)],
-    ['Spike caves can spawn.']
-);
-
-addCombo([notTheBees],
-    ['No shell piles can spawn.']
 );
 
 addCombo([seedNot(skyblock), noTraps, seedOr([celebrationMk10, notTheBees])],
@@ -105,10 +100,6 @@ addCombo([seedNot(celebrationMk10), seedNot(worldIsInfected), seedNot(vampirism)
     ['The guide is replaced by a merchant.']
 );
 
-addCombo([notTheBees, seedNot(remix)],
-    ['No thin ice mini-biomes spawn.', 'No campsites mini-biomes spawn.', 'No mining explosive mini-biomes spawn.']
-);
-
 addCombo([seedOr([forTheWorthy, noTraps]), seedNot(notTheBees), seedNot(actuallyNoTraps)],
     ['50% more explosive traps spawn.']
 );
@@ -145,18 +136,171 @@ addCombo([celebrationMk10, seedNot(notTheBees), drunk],
     ['The Jungle Temple is painted green.']
 );
 
-addCombo([notTheBees, seedNot(theConstant), seedNot(skyblock), seedNot(drunk), seedNot(celebrationMk10), seedNot(remix)],
-    ['Most of the world is converted to Jungle.']
-);
-
-addCombo([notTheBees, seedOr([theConstant, drunk, celebrationMk10]), seedNot(remix), seedNot(skyblock)],
-    ['Most of the world is converted to Jungle, except for the middle portion of the world.']
+addCombo([notTheBees, seedOr([seedNot(theConstant), remix])],
+    ['Slush blocks are converted to honey.']
 );
 
 addCombo([notTheBees, remix],
-    ['A rectangular strip of hive spawns near the top of the world, stretching across almost the entire world (except the oceans). A strip of Crispy Honey Block spawns underneath that.']
+    ['Water in the hive/crispy strips turns to honey except for the central portion of the hive strip.']
 );
 
-addCombo([notTheBees, seedOr(seedNot(theConstant), remix)],
-    ['Slush blocks are converted to honey.']
+addCombo([remix, forTheWorthy],
+    ['Liquids on the surface turn to honey.']
+);
+
+addCombo([notTheBees, remix, seedNot(forTheWorthy)],
+    ['Water on the surface also turns to honey.']
+);
+
+addCombo([seedNot(notTheBees), remix, seedNot(forTheWorthy), drunk, celebrationMk10],
+    ['Water on the surface on the Dungeon side turns into Shimmer.']
+);
+
+addCombo([notTheBees, seedNot(theConstant), seedNot(remix), seedNot(celebrationMk10)],
+    ['Most water in the world turns in to Honey except for the middle 1/6 of the world and the outer 1/6s of the world.']
+);
+
+addCombo([notTheBees, theConstant, seedNot(remix)],
+    ['Some water in the world is converted to Honey.']
+);
+
+addCombo([seedNot(notTheBees), remix, drunk, celebrationMk10],
+    ['Some water throughout world is converted to Shimmer.']
+);
+
+addCombo([seedNot(notTheBees), seedNot(seedAnd([remix, drunk, celebrationMk10])), forTheWorthy],
+    ['Some water throughout world is converted to Lava.']
+);
+
+addCombo([notTheBees, forTheWorthy, seedNot(celebrationMk10)],
+    ['Every Underworld House is made of Crispy Honey Block.', 'Spiders are 20% less likely to spawn in Underworld Spider Nests compared to regular ones.']
+);
+
+addCombo([notTheBees, seedOr([theConstant, noTraps])],
+    ['Spikes are replaced with Wooden Spikes.']
+);
+
+addCombo([notTheBees, seedOr([theConstant, remix])],
+    ['A few Larvae are scattered around the world.']
+);
+
+addCombo([notTheBees, noTraps, seedNot(theConstant), seedNot(remix)],
+    ['Very many Larvae are scattered around the world.']
+);
+
+addCombo([notTheBees, seedNot(noTraps), seedNot(theConstant), seedNot(remix)],
+    ['Many Larvae are scattered around the world.']
+);
+
+addCombo([notTheBees, theConstant, seedNot(remix), secretSeedsCount(4,-1, noSpiderNests), seedNot(forTheWorthy)],
+    ['Numerous spider nests are added.']
+);
+
+addCombo([notTheBees, theConstant, seedNot(remix), secretSeedsCount(4,-1, noSpiderNests), forTheWorthy],
+    ['A very large number of spider nests are added.']
+);
+
+addCombo([notTheBees, seedNot(remix)],
+    ['The Dungeon is overgrown with Jungle grass and trees. Its walls are painted orange.']
+);
+
+addCombo([forTheWorthy, seedNot(drunk), seedNot(notTheBees)],
+    ['Dungeon tiles are painted with a random color.']
+);
+
+addCombo([forTheWorthy, seedNot(notTheBees)],
+    ['The Jungle Temple is painted green.']
+);
+
+addCombo([notTheBees, seedNot(remix)],
+    ['The merchant sells Jungle Torches.']
+);
+
+addCombo([notTheBees, celebrationMk10, seedNot(remix)],
+    ['The merchant sells the Furnace even when not in the Jungle.']
+);
+
+addCombo([drunk, celebrationMk10, seedNot(remix), seedNot(notTheBees)],
+    ['Half of the world generates with Shimmer replacing water.', 'The Dungeon is buried but no tree is visible above it.', 'There is no Dungeon Guardian, and Skeletron does not have to be fought to enter the Dungeon.', 'Palm trees drop triple wood and ash trees drop triple wood.', 'The Party Girl is replaced by a Painter named Jim', 'Slimefors is invincible, with maximum health, 99999 defense, and inability to be attacked.', 'Snow (Otherworldly) will play during world generation, the background will be the Hallow, and heart-shaped clouds will appear.', 'Sand is painted rainbow and sand walls are ainted black.']
+);
+
+
+addCombo([drunk, seedNot(remix), seedNot(celebrationMk10), vampirism],
+    ['Crimson (Otherworldly) will play during world generation.']
+);
+
+addCombo([drunk, remix],
+    ['Space (Otherworldly) will play during World Generation.']
+);
+
+addCombo([drunk, seedNot(remix), seedNot(celebrationMk10), seedNot(vampirism), notTheBees],
+    ['Jungle (Otherworldly) will play during world generation.']
+);
+
+addCombo([drunk, seedNot(remix), seedNot(celebrationMk10), seedNot(vampirism), seedNot(notTheBees)],
+    ['The Console Title Music will play during world generation.']
+);
+
+addCombo([remix],
+    ['The logo flips upside down and rotates during world generation.']
+);
+
+addCombo([seedNot(remix), drunk, seedNot(notTheBees)],
+    ['The rotates during world generation.']
+);
+
+addCombo([drunk, seedOr([seedNot(celebrationMk10), notTheBees, remix]), vampirism],
+    ['The background during world generation is the Crimson.']
+);
+
+addCombo([drunk, seedNot(vampirism), notTheBees],
+    ['The background during world generation is the Jungle.']
+);
+
+addCombo([notTheBees, seedNot(forTheWorthy)],
+    ['All world generation messages are replaced with \'Generating Bees\'.']
+);
+
+addCombo([drunk, celebrationMk10],
+    ['Underground Cabins can spawn with Slime Furniture.']
+);
+
+addCombo([notTheBees, celebrationMk10],
+    ['Underground Cabins can spawn with Bamboo or Honey Furniture.']
+);
+
+addCombo([seedOr([skyblock, roundLandmasses, noSurface, notTheBees]), seedNot(theConstant)],
+    ['The Oasis mini biome cannot spawn.']
+);
+
+addCombo([seedOr([skyblock, roundLandmasses, noSurface]), theConstant],
+    ['The Oasis mini biome cannot spawn.']
+);
+
+addCombo([noTraps, forTheWorthy, seedNot(celebrationMk10), seedNot(notTheBees)],
+    ['Spike pits can spawn outside of the dungeon.']
+);
+
+addCombo([noTraps, forTheWorthy, seedNot(celebrationMk10), notTheBees],
+    ['Wooden spike pits can spawn outside the Jungle Temple.']
+);
+
+addCombo([notTheBees],
+    ['No shell piles can spawn.', 'Half of fishing attempts in honey will result in water-type fish.']
+);
+
+addCombo([notTheBees, seedNot(remix)],
+    ['No thin ice mini-biomes spawn.', 'No campsites mini-biomes spawn.', 'No mining explosive mini-biomes spawn.', 'Half of the time, fishing will ignore the Jungle when calculating the current biome.']
+);
+
+addCombo ([drunk, seedOr([seedNot(celebrationMk10), remix, notTheBees])],
+	['A living tree painted brown will spawn over the dungeon.']
+);
+
+addCombo ([drunk, notTheBees],
+	['A living tree made of Mahogany will spawn over the dungeon.']
+);
+
+addCombo([seedOr([notTheBees, noTraps, pooEverywhere]), secretSeedsCount(4,-1, actuallyNoTraps)],
+    ['Poo boulders may spawn.']
 );
